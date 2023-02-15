e4ff421bc921c5c482edec546cc43949aa673e3a
Ritu Saini <121332945+ritu25685@users.noreply.github.com>
2023-02-15 19:54:43 +0530

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+ezgif.com-gif-maker.mp4
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -5,18 +5,28 @@ import 'semantic-ui-css/semantic.min.css';
 function CounterApp() {
   const [count, setCount] = useState(0);
 
-  const incrementCount = () => setCount(count + 1);
+  const incrementCount = () =>{
+    if(count===10) return;
+     setCount(count + 1);
+  }
   const decrementCount = () => {
     if (count === 0) return;
     setCount(count - 1);
   };
+  let color = "green";
+  if (count >= 5 && count <= 9) {
+    color = "blue";
+  } else if (count === 10) {
+    color = "red";
+  }
+
 
   return (
     <>
     
     <Segment raised style={{margin: '100px auto', width: '500px'}}>
       <Grid.Row>
-        <h2 style={{textAlign: 'center', margin:'50px auto'}}>Count</h2>
+        <h2 style={{textAlign: 'center', margin:'50px auto',color:color}}>Count</h2>
       </Grid.Row>
       <Grid.Row>
           <h2 style={{textAlign: 'center',margin:'50px auto'}}>{count}</h2>
